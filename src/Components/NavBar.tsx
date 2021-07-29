1d50ae7f46120578e0046713759e68792e371834
Peter Sun <pitaah@umich.edu>
2021-07-29 08:31:08 -0400

    - Added socials

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 import './NavBar.css';
+import { Icon } from '@iconify/react';
+import githubIcon from '@iconify-icons/logos/github-icon';
+import linkedinFill from '@iconify-icons/akar-icons/linkedin-fill';
 
 function NavBar(props: { sections: Array<string> }) {
   const [navState, setNavState] = useState({
@@ -23,6 +26,12 @@ function NavBar(props: { sections: Array<string> }) {
             {elem}
         </a>;
       })}
+      <a className="socials" href="https://www.linkedin.com/in/peterysun/" target="_blank">
+        <Icon icon={linkedinFill} />
+      </a>
+      <a className="socials" href="https://github.com/Narnian12" target="_blank">
+        <Icon icon={githubIcon} />
+      </a>
     </div>
   );
 }
--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -15,6 +15,25 @@ function Profile() {
         understanding of UI development with the React library.
         </p>
       </div>
+      <div className="card">
+        <h2>Work</h2>
+        <p>I am currently a software engineer in Promess, a manufacturing company specializing
+          in sensing systems. For a few years, I was heavily
+          involved in creating full-stack features using the .NET framework. More recently,
+          I am working on developing with a new tech stack using Rust and React.
+        </p>
+      </div>
+      <div className="card">
+        <h2>Education</h2>
+        <p>I am pursuing a Master of Science in Computer Science from Oakland University.
+          More informally, I have been completing certifications from 
+          <a href="https://www.freecodecamp.org/learn" target="_blank"> freeCodeCamp</a>.
+        </p>
+      </div>
+      <div className="card">
+        <h2>Research</h2>
+        <p></p>
+      </div>
     </div>
   );
 }
